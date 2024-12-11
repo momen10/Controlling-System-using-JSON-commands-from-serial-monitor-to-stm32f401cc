a4dad4b974db70519705594bac3a11f575267b27
momen10 <93386244+momen10@users.noreply.github.com>
2024-12-11 18:36:53 +0200

    Add files via upload

---
+++ src/MCAL/GPIO/GPIO_config.h
@@ -0,0 +1,19 @@
+/*
+   NAME        : GPIO_config.h
+
+   AUTHOR      : MO'MEN AHMED
+
+   DATE        : 3/7/2023
+
+   DESCRIPTION : COnfiguration header file for GPIO driver
+ */
+
+
+#ifndef MCAL_GPIO_GPIO_CONFIG_H_
+#define MCAL_GPIO_GPIO_CONFIG_H_
+
+
+
+
+
+#endif /* MCAL_GPIO_GPIO_CONFIG_H_ */
